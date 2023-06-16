0d3f64343bc19c9847424a8d47e6753578864924
KHoaDangggg <khoa.dp210486@sis.hust.edu.vn>
2023-06-16 22:53:51 +0700

    deploy fix

--- test.mjs
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-import Store from './models/storeModel.mjs';
-(async () => {
-    const stores = await Store.find();
-    const ids = stores.map((el) => el.id);
-    console.log(ids);
-})();
